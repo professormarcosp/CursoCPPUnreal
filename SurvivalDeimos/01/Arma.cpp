d115e53c58a5fe7b1a212806609c2beba50010eb
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2018-11-28 11:05:24 -0300

    Add files via upload

--- SurvivalDeimos/01/Arma.cpp
+++ SurvivalDeimos/01/Arma.cpp
@@ -1,1 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Arma.h"
+#include "Components/SkeletalMeshComponent.h"
+
+
+// Sets default values
+AArma::AArma()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	MalhaDaArma = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Malha da Arma"));
+
+}
+
+// Called when the game starts or when spawned
+void AArma::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AArma::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
 
