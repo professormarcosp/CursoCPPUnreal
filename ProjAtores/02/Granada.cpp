68670e31afa471f4200c50485a83fa4c5411bdbf
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-01-20 23:45:45 -0300

    Add files via upload

--- ProjAtores/02/Granada.cpp
+++ ProjAtores/02/Granada.cpp
@@ -1,1 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Granada.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "Engine/EngineTypes.h"
+#include "UObject/ConstructorHelpers.h"
+
+
+// Sets default values
+AGranada::AGranada()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	ColisorDoAtor = CreateDefaultSubobject<USphereComponent>(FName("Colisor"));
+	ColisorDoAtor->SetSphereRadius(12.f);
+	ColisorDoAtor->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	RootComponent = ColisorDoAtor;
+	
+	MalhaDoAtor = CreateDefaultSubobject<UStaticMeshComponent>(FName("Malha"));
+	MalhaDoAtor->SetRelativeLocation(FVector(3.2f, 0.f, 0.f));
+	MalhaDoAtor->SetRelativeScale3D(FVector(0.5f, 0.5f, 0.5f));
+	MalhaDoAtor->SetupAttachment(RootComponent);
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh>Granada(TEXT("StaticMesh'/Game/FBX/granada.granada'"));
+
+	if (Granada.Succeeded())
+	{
+		MalhaDoAtor->SetStaticMesh(Granada.Object);
+	}
+
+
+}
+
+// Called when the game starts or when spawned
+void AGranada::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGranada::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
 
