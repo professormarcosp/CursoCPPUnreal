852e57fe094533320b13af4b20fcf4ee27b84bd2
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-01-23 22:37:46 -0300

    Add files via upload

---
+++ ProjAtores/06/MeuGameMode.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MeuGameMode.h"
+#include "Engine/TargetPoint.h"
+#include "Kismet/GameplayStatics.h"
+#include "Granada.h"
+#include "Engine/World.h"
+#include "Engine/Engine.h"
+
+void AMeuGameMode::StartPlay()
+{
+	Super::StartPlay();
+	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("Estou no Game Mode \n Granadas Lançadas"));
+}
+
+
+void AMeuGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	TArray<AActor*> LocalGranadas;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ATargetPoint::StaticClass(), LocalGranadas);
+	//UE_LOG(LogTemp, Warning, TEXT("Numero de TargetPoints Encontrados %d"), LocalGranadas.Num());
+
+	FActorSpawnParameters params = FActorSpawnParameters();
+
+	for (auto It : LocalGranadas)
+	{
+		GetWorld()->SpawnActor<AGranada>(It->GetActorLocation(), It->GetActorRotation(), params);
+		//GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Red, TEXT("Ator Gerado"));
+		//UE_LOG(LogTemp, Warning, TEXT("Transform = %s"), *It->GetActorLocation().ToString());
+	}
+
+}
+
+
+
+
+
+
