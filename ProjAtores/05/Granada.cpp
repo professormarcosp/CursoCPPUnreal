91d91cd2314e4e265d04156d07dca77a94294fd1
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-01-23 22:11:19 -0300

    Add files via upload

--- ProjAtores/05/Granada.cpp
+++ ProjAtores/05/Granada.cpp
@@ -1,1 +1,88 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Granada.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "Engine/EngineTypes.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Components/AudioComponent.h"
+#include "TimerManager.h"
+#include "Sound/SoundCue.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Engine/Engine.h"
+
+
+// Sets default values
+AGranada::AGranada()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	ColisorDoAtor = CreateDefaultSubobject<USphereComponent>(FName("Colisor"));
+	ColisorDoAtor->SetSphereRadius(12.f);
+	ColisorDoAtor->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	RootComponent = ColisorDoAtor;
+	
+	MalhaDoAtor = CreateDefaultSubobject<UStaticMeshComponent>(FName("Malha"));
+	MalhaDoAtor->SetRelativeLocation(FVector(3.2f, 0.f, 0.f));
+	MalhaDoAtor->SetRelativeScale3D(FVector(0.5f, 0.5f, 0.5f));
+	MalhaDoAtor->SetupAttachment(RootComponent);
+
+	SomDaExplosao = CreateDefaultSubobject<UAudioComponent>(FName("Som Da Explosao"));
+	SomDaExplosao->bAutoActivate = false;
+	SomDaExplosao->bAutoDestroy = true;
+	SomDaExplosao->SetupAttachment(MalhaDoAtor);
+
+	Explosao = CreateDefaultSubobject<UParticleSystemComponent>(FName("ParticulaExplosao"));
+	Explosao->bAutoActivate = false;
+	Explosao->bAutoDestroy = true;
+	Explosao->SetupAttachment(MalhaDoAtor);
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh>Granada(TEXT("StaticMesh'/Game/FBX/granada.granada'"));
+
+	ConstructorHelpers::FObjectFinder<USoundCue>SomGranada(TEXT("SoundCue'/Game/StarterContent/Audio/Explosion_Cue.Explosion_Cue'"));
+
+	ConstructorHelpers::FObjectFinder<UParticleSystem>ParticulaExplosao(TEXT("ParticleSystem'/Game/StarterContent/Particles/P_Explosion.P_Explosion'"));
+
+	if (Granada.Succeeded() && SomGranada.Succeeded() && ParticulaExplosao.Succeeded())
+	{
+		MalhaDoAtor->SetStaticMesh(Granada.Object);
+		SomDaExplosao->SetSound(SomGranada.Object);
+		Explosao->SetTemplate(ParticulaExplosao.Object);
+	}
+
+
+}
+
+void AGranada::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Granda Destruida"));
+	GetWorldTimerManager().ClearTimer(Temporizador);
+}
+
+// Called when the game starts or when spawned
+void AGranada::BeginPlay()
+{
+	Super::BeginPlay();
+	MalhaDoAtor->SetSimulatePhysics(true);
+	MalhaDoAtor->SetEnableGravity(true);
+	GetWorldTimerManager().SetTimer(Temporizador, this, &AGranada::ExplodirGranada, 5.f, false);
+	
+}
+
+void AGranada::ExplodirGranada()
+{
+	SetLifeSpan(5.5f);
+	SomDaExplosao->Play();
+	Explosao->ActivateSystem();
+	MalhaDoAtor->SetVisibility(false);
+}
+
+// Called every frame
+void AGranada::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
 
--- ProjAtores/05/MeuGameMode.h
+++ ProjAtores/05/MeuGameMode.h
@@ -1,1 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
 
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MeuGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJATORES_API AMeuGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void BeginPlay() override;
+	virtual void StartPlay() override;
+
+	
+	
+};
