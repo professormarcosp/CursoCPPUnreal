9daf942cfe5bcd967c55496176c411c81b2de1a2
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-01-24 00:03:29 -0300

    Add files via upload

--- ProjAtores/08/Granada.h
+++ ProjAtores/08/Granada.h
@@ -1,1 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
 
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Granada.generated.h"
+
+UCLASS()
+class PROJATORES_API AGranada : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGranada();
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void TempoDevagar(float DilatacaoDoTempo);
+
+	UFUNCTION()
+	void RestaurarTempo();
+
+	UFUNCTION()
+	void ImpulsoRadial(float Raio, float Forca);
+
+	
+	FTimerHandle Temporizador, TimerDoTempo;
+
+	class UAudioComponent* SomDaExplosao;
+
+	class UParticleSystemComponent* Explosao;
+
+	UFUNCTION()
+	void ExplodirGranada();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Granada")
+	class UStaticMeshComponent* MalhaDoAtor;
+
+	class USphereComponent* ColisorDoAtor;
+	
+};
